0fa879b54f4b9f8361ca134042a29e4a375b2cee
IKEENOO <kudryavtsev.dev@gmail.com>
2024-02-14 23:44:19 +0300

    feat: update structure

---
+++ code/src/pages/Main/main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./main.css";
+
+export class Main extends Component {
+    render() {
+        return (
+            <>
+                <h1>KUDRYAVTSEV</h1>
+                <h2>frontend developer</h2>
+            </>
+        );
+    }
+}
